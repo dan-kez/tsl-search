85c7789c3f39e8f949a6338d4dbf8a2da03c1bb5
Daniel Kezerashvili <daniel.kez@gmail.com>
2023-09-12 23:47:11 -0400

    update to NEXT_PUBLIC_VERCEL_URL

--- src/Login.tsx
+++ src/Login.tsx
@@ -5,7 +5,7 @@ import NavBar from './NavBar';
 const getURL = () => {
   let url =
     import.meta.env.VITE_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    import.meta.env.VITE_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    import.meta.env.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
     'http://localhost:5173/';
   // Make sure to include `https://` when not localhost.
   url = url.includes('http') ? url : `https://${url}`;
