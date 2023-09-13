d6d27bdcf51aeeaf18a341441fc9215e8479d804
Daniel Kezerashvili <daniel.kez@gmail.com>
2023-09-12 23:45:12 -0400

    update redirect url

--- src/Login.tsx
+++ src/Login.tsx
@@ -2,6 +2,18 @@ import { useState } from 'react';
 import { supabase } from './supabase/supabaseClient';
 import NavBar from './NavBar';
 
+const getURL = () => {
+  let url =
+    import.meta.env.VITE_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    import.meta.env.VITE_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:5173/';
+  // Make sure to include `https://` when not localhost.
+  url = url.includes('http') ? url : `https://${url}`;
+  // Make sure to include a trailing `/`.
+  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+  return url;
+};
+
 export default function Auth() {
   const [loading, setLoading] = useState(false);
 
@@ -11,6 +23,9 @@ export default function Auth() {
     setLoading(true);
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'discord',
+      options: {
+        redirectTo: getURL(),
+      },
     });
 
     if (error) {
